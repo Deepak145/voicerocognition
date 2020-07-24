d8eb7685e2a6a99cd00b3f2fa712eb8b9dab59d6
unknown <dubey.deepak145@gmail.com>
2020-07-24 16:21:52 +0530

    removed test function from directv.js

--- src/directv.js
+++ src/directv.js
@@ -9,10 +9,3 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.listen(9000, () => {
     console.log("Server is running")
 })
-
-
-function test(comment) {
-    console.log("test called....");
-}
-
-export{test}
